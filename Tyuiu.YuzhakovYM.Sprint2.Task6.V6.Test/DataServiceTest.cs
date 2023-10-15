c3dd4cea80c188f9d2226178a92f161c5e962292
Tyuiu.YuzhakovYM@outlook.com <yuzhakov3@inbox.ru>
2023-10-15 22:09:23 +0300

    Task6

---
+++ Tyuiu.YuzhakovYM.Sprint2.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.YuzhakovYM.Sprint2.Task6.V6.Lib;
+
+namespace Tyuiu.YuzhakovYM.Sprint2.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardNameAndValue()
+        {
+            DataService ds = new DataService();
+            int m = 1;
+            int k = 14;
+            string res = ds.FindCardNameAndValue(m, k);
+            string wait = "Туз пики";
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.YuzhakovYM.Sprint2.Task6.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.YuzhakovYM.Sprint2.Task6.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.YuzhakovYM.Sprint2.Task6.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c653f80a-78cb-4aef-a471-462cf1e7606a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
